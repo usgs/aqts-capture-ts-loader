bf51732c0dd2c302b3961213620370c8ac64f184
ssoper <ssoper@usgs.gov>
2020-04-01 11:42:37 -0500

    delete and insert are transactional

--- src/main/java/gov/usgs/wma/waterdata/ObservationDao.java
+++ src/main/java/gov/usgs/wma/waterdata/ObservationDao.java
@@ -10,6 +10,7 @@ import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.PreparedStatementSetter;
 import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.util.FileCopyUtils;
 
 import java.io.IOException;
@@ -36,6 +37,7 @@ public class ObservationDao {
 	@Value("classpath:sql/insertTimeSeries.sql")
 	protected Resource insertQuery;
 
+	@Transactional
 	public Integer deleteTimeSeries(String timeSeriesUniqueId) {
 		Integer rowsDeletedCount = null;
 		try {
@@ -53,7 +55,7 @@ public class ObservationDao {
 		return rowsDeletedCount;
 	}
 
-
+	@Transactional
 	public Integer insertTimeSeries(TimeSeries timeSeries) {
 		Integer rowsInsertedCount = null;
 		try {
